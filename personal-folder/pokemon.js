084eceb1d9ce4bcf7c3fe173f6cba4b9b44e7e5b
faerbers <shelby907gw@byui.edu>
2026-03-30 18:12:54 -0700

    project-start

---
+++ personal-folder/pokemon.js
@@ -0,0 +1,29 @@
+const starters = [{
+        pokemon: 'Browt',
+        category: 'Bean Chick Pokemon',
+        type: 'Grass',
+        height:  '1\'',  
+        weight: '7.7lbs', 
+        ability: 'Overgrow',
+        overview: 'This Pokémon runs about energetically while photosynthesizing using the leaves on its brow. It\'s lively,but it can also be a bit clumsy.'
+    },
+{		pokemon: 'Pombon', 
+		category: 'Puppy Pokemon',
+		type: 'Fire',
+		height: '1\'4\'\'',
+		weight: '14.8lbs',
+		ability: 'Blaze',
+		overview:'The area below its throat glows faintly from the heat-generating organ within its lungs. This Pokémon is guileless and friendly.'
+    },
+{			
+	    pokemon: 'Gecqua',
+		Category: 'Water Gecko Pokemon',
+		Type: 'Water',
+		Height: '1\'',
+		Weight: '9.5lbs',
+		Ability: 'Torrent',
+		Overview: 'This Pokémon launches springy balls of water from its tail. Gecqua is very intelligent and maneuvers shrewdly while putting on airs.'
+    }
+];
+
+	
