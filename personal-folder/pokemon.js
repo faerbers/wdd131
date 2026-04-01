f1f3b5974b51d9e1c6f6ce967200341de9465ec7
faerbers <shelby907gw@byui.edu>
2026-03-31 18:51:59 -0700

    final

--- personal-folder/pokemon.js
+++ personal-folder/pokemon.js
@@ -1,5 +1,6 @@
 const starters = [{
-        pokemon: 'Browt',
+        name: 'Browt',
+		image: 'images/browt.webp',
         category: 'Bean Chick Pokemon',
         type: 'Grass',
         height:  '1\'',  
@@ -7,7 +8,8 @@ const starters = [{
         ability: 'Overgrow',
         overview: 'This Pokémon runs about energetically while photosynthesizing using the leaves on its brow. It\'s lively,but it can also be a bit clumsy.'
     },
-{		pokemon: 'Pombon', 
+{		name: 'Pombon', 
+		image: 'images/pombon.webp',
 		category: 'Puppy Pokemon',
 		type: 'Fire',
 		height: '1\'4\'\'',
@@ -16,14 +18,59 @@ const starters = [{
 		overview:'The area below its throat glows faintly from the heat-generating organ within its lungs. This Pokémon is guileless and friendly.'
     },
 {			
-	    pokemon: 'Gecqua',
-		Category: 'Water Gecko Pokemon',
-		Type: 'Water',
-		Height: '1\'',
-		Weight: '9.5lbs',
-		Ability: 'Torrent',
-		Overview: 'This Pokémon launches springy balls of water from its tail. Gecqua is very intelligent and maneuvers shrewdly while putting on airs.'
+	    name: 'Gecqua',
+		image: 'images/gecqua.webp',
+		category: 'Water Gecko Pokemon',
+		type: 'Water',
+		height: '1\'',
+		weight: '9.5lbs',
+		ability: 'Torrent',
+		overview: 'This Pokémon launches springy balls of water from its tail. Gecqua is very intelligent and maneuvers shrewdly while putting on airs.'
     }
 ];
 
-	
+
+function starterTemplate(pokemon) {
+	return `<img src="${pokemon.image}" alt="pokeball">
+            <h3>${pokemon.name}</h3>
+            <ul>
+                <li>Category: ${pokemon.category}</li>
+                <li>Type: ${pokemon.type}</li>
+                <li>Height: ${pokemon.height}</li>
+                <li>Weight: ${pokemon.weight}</li>
+                <li>Ability: ${pokemon.ability}</li>
+                <li>Overview: ${pokemon.overview}</li>
+            </ul>`
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const pokeballDiv = document.querySelector('.pokeball');
+    const basicPokeball = document.getElementById('basic-pokeball');
+    
+    const grassBtn = document.querySelector('#grass');
+    const fireBtn = document.querySelector('#fire');
+    const waterBtn = document.querySelector('#water');
+
+
+    function showStarter(pokemon) {
+        basicPokeball.style.display = 'none';     
+        pokeballDiv.innerHTML = starterTemplate(pokemon);
+    }
+
+    // Grass button
+    grassBtn.addEventListener('click', () => {
+        showStarter(starters[0]);
+    });
+
+    // Fire button
+    fireBtn.addEventListener('click', () => {
+        showStarter(starters[1]);
+    });
+
+    // Water button
+    waterBtn.addEventListener('click', () => {
+        showStarter(starters[2]);
+    });
+});
